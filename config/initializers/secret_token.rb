bdf840d8a28233bb94814ca25521bd96fe76625f
John Cowhig Jr <jcowhigr+dev@gmail.com>
2014-06-03 14:09:03 -0400

    basically regenerated some boilerplate that was missing: rails-api new .

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EmberRailsAPI::Application.config.secret_token = '09c76361dca2c4897d430ccabf46310b918d140160c2b27c6fceaf5155cc0c14d7c3e761aa91b8ce09a80ff2a0d07ef85c1b0625800f563d7b981ddc423c7406'
